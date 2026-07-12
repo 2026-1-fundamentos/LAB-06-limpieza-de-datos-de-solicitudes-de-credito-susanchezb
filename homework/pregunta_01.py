00433b1145a0df4f40c6ccc22305013999a3a38d
SusanaSanchez12 <susanchezb@unal.edu.co>
2026-07-11 22:17:01 -0500

    INT1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -16,9 +16,9 @@ def pregunta_01():
 
     """
 
-df = pd.read_csv("files/input/solicitudes_de_credito.csv", sep = ";", index_col = 0) #? Se lee el archivo
+df = pd.read_csv("files/input/solicitudes_de_credito.csv", sep = ";", index_col = 0) # Se lee el archivo
     
-for column in df.select_dtypes(include = ["object"]).columns:                        #?Limpieza de los campos
+for column in df.select_dtypes(include = ["object"]).columns:                        # Limpieza de los campos
     df[column] = df[column].str.lower()
     df[column] = df[column].str.replace("_", " ")
     df[column] = df[column].str.replace("-", " ")
@@ -26,14 +26,14 @@ for column in df.select_dtypes(include = ["object"]).columns:                   
     df[column] = df[column].str.replace("$", "")
     df[column] = df[column].str.replace(".00", "")
 
-df["monto_del_credito"] = df["monto_del_credito"].astype(float)                      #?pasar monto_del_credito a flotante
-df["comuna_ciudadano"]  = df["comuna_ciudadano"].astype(int)                         #?Pasar comuna_ciudadano a entero 
-df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"],                  #? oRGANIZAR LOS FORMATOS DE LA FECHAS     
+df["monto_del_credito"] = df["monto_del_credito"].astype(float)                      # pasar monto_del_credito a flotante
+df["comuna_ciudadano"]  = df["comuna_ciudadano"].astype(int)                         # Pasar comuna_ciudadano a entero 
+df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"],                  # ORGANIZAR LOS FORMATOS DE LA FECHAS     
                                                 format = "%d/%m/%Y", errors = "coerce").combine_first(pd.to_datetime(df["fecha_de_beneficio"],
                                                 format = "%Y/%m/%d", errors = "coerce"))
-df = df.drop_duplicates()                                                            #? Sin duplicados en el dataframe
-df = df.dropna()                                                                     #? Sin registros nulos
+df = df.drop_duplicates()                                                            # Sin duplicados en el dataframe
+df = df.dropna()                                                                     # Sin registros nulos
 
-os.makedirs("files/output", exist_ok = True)                                         #? Carpeta output ppara el test
+os.makedirs("files/output", exist_ok = True)                                         # Carpeta output ppara el test
 
 df.to_csv("files/output/solicitudes_de_credito.csv", columns = df.columns, index = False, encoding = "utf-8", sep = ";")
